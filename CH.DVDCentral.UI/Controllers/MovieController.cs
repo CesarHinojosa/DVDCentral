21e2506099cdfb2d0591930cf72ec21001ac04af
Cesar Hinojosa Bautista <300077836@fvtc.edu>
2023-10-17 16:56:45 -0500

    Added Director, Format, Genre, and Movie Managers as well as the views

---
+++ CH.DVDCentral.UI/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CH.DVDCentral.UI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(MovieManager.Load());
+        }
+    }
+}
