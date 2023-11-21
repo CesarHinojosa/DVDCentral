54fd4b677f6a1d5a318fedc70faf3e2e487cce2a
Cesar Hinojosa Bautista <300077836@fvtc.edu>
2023-11-21 14:38:10 -0600

    Modified Customer ORder Partial

--- CH.DVDCentral.UI/ViewModels/CustomerOrders.cs
+++ CH.DVDCentral.UI/ViewModels/CustomerOrders.cs
@@ -2,5 +2,8 @@
 {
     public class CustomerOrders
     {
+        public List<Customer> Customer { get; set; } = new List<Customer>();
+
+        public List<Order> Orders { get; set; } = new List<Order>();
     }
 }
