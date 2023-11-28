6556bcbc1d10c6b684a83a7b375d6fa4023a8d38
Cesar Hinojosa Bautista <300077836@fvtc.edu>
2023-11-28 10:08:41 -0600

    Fixed Client Side javascript to show image imeediately

--- CH.DVDCentral.UI/wwwroot/js/site.js
+++ CH.DVDCentral.UI/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+<script>
+    var loadfile = function (event) {
+        var output = document.getElementById('movieimage');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    }
+</script>
